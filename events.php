d9678e6ff2cbcd20b48407f5e5e126cd28ecbb0e
idropepon1324 <eralntos@csd.auth.gr>
2022-05-28 13:09:16 +0300

    Small changes

--- events.php
+++ events.php
@@ -25,39 +25,48 @@
                         </form>";
                 }
               }
-             ?>
 
-            <div class="image-container">
-              <?php
-                $counter = 0;
-                $result = $conn->query($sql);
-                if ($result->num_rows > 0) {
-                  while ($row = $result->fetch_assoc()){
-                    container($row["title"], $row["description"], $row["date"], $row["url"], $row["image"], $counter);
-                    $counter++;
-                    if ($counter>=$MAX_POST) {    // Max 8 News at a time
-                      break;
+              // if an user is connected show it else not
+              if (!isset($_SESSION["email"])){
+                echo "<h2> Πρέπει να συνδεθείτε για να μπορέσετε να δείτε τις εκδηλώσεις μας!</h2>
+                      <center>
+                        <form type=\"post\" action=\"login.php\">
+                          <button type=\"submit\">Σύνδεση</button>
+                        </form>
+                      </center>";
+              } else {
+                echo "<div class=\"image-container\">";
+                  $counter = 0;
+                  $result = $conn->query($sql);
+                  if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()){
+                      container($row["title"], $row["description"], $row["date"], $row["url"], $row["image"], $counter);
+                      $counter++;
+                      if ($counter>=$MAX_POST) {    // Max 8 News at a time
+                        break;
+                      }
                     }
+                  } else {
+                    echo "<h3>No News available!<h3>";
                   }
-                } else {
-                  echo "<h3>No News available!<h3>";
                 }
 
-                function container($title, $desc, $date, $url, $image, $position){
-                  echo "<div class=\"image\"><img src=\"images/$image\" alt=\"image\" onclick=\"showImage('$position');\"> <p>$title</p></div>
-                  <!--text box for pic1-->
-                  <div class=\"popup-window\" id=\"$position\">
-                      <div class=\"close-btn\" onclick=\"closeImage('$position')\">&times;</div>
-                      <div class=\"white-board\">
-                          <h2> $title</h2>
-                          <p>$date<br>
-                             $desc
-                          </p>
-                          <p class=\"text-source\">Πηγή:$url</p>
-                      </div>
-                  </div>";
-                }
-               ?>
+              function container($title, $desc, $date, $url, $image, $position){
+                echo "<div class=\"image\"><img src=\"images/$image\" alt=\"image\" onclick=\"showImage('$position');\"> <p>$title</p></div>
+                <!--text box for pic1-->
+                <div class=\"popup-window\" id=\"$position\">
+                    <div class=\"close-btn\" onclick=\"closeImage('$position')\">&times;</div>
+                    <div class=\"white-board\">
+                        <h2> $title</h2>
+                        <p>$date<br>
+                           $desc
+                        </p>
+                        <p class=\"text-source\">Πηγή:$url</p>
+                    </div>
+                </div>";
+              }
+
+             ?>
              </div>
         </div>
 
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
       <meta name="viewport" content="with=device-width, initial-scale=1.0">
       <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1" /> 
       <title>Galindo's Green Plan</title>
       <link rel="stylesheet" href="CSS/style.css">
   </head>
